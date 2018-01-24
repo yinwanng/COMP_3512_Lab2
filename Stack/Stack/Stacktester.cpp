24da9b4a2300b18cfc2ffbfb9dc67d2993adb55a
yinwanng <xxjohnnyng@gmail.com>
2018-01-24 00:20:55 -0800

    Fix pop member function as it required a error check.

--- Stack/Stack/Stacktester.cpp
+++ Stack/Stack/Stacktester.cpp
@@ -6,7 +6,17 @@ using std::endl;
 
 int main()
 {
-	cout << "Hello world! " << endl;
+	Stack s1;
+	Stack s2;
+
+	s1.print();
+	s1.push(1);
+	s1.print();
+	s1.pop();
+	s1.print();
+
+
+
 	system("pause");
 	return 0;
 }
