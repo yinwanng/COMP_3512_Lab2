48b4b3d4c87abb6b0eb443a45244fe351f08b68b
yinwanng <xxjohnnyng@gmail.com>
2018-01-24 12:22:22 -0800

    Added push print to testing.

--- Stack/Stack/Stacktester.cpp
+++ Stack/Stack/Stacktester.cpp
@@ -25,6 +25,7 @@ int main()
 	s1.pop();
 	cout << "The pop function has been invoked." << endl;
 	s1.push(10);
+	cout << "The value of 10 has been pushed to the stack." << endl;
 	s1.print();
 
 	system("pause");
