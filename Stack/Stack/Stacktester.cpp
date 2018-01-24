b2ff6db2870f8f8372d6d90ae20e9d78dc2d84c6
yinwanng <xxjohnnyng@gmail.com>
2018-01-24 12:33:12 -0800

    Testing the full data function to work.

--- Stack/Stack/Stacktester.cpp
+++ Stack/Stack/Stacktester.cpp
@@ -28,6 +28,16 @@ int main()
 	cout << "The value of 10 has been pushed to the stack." << endl;
 	s1.print();
 
+	cout << "There will be 10 values in the stack and one has not been added." << endl;
+	s1.push(4);
+	s1.push(5);
+	s1.push(6);
+	s1.push(7);
+	s1.push(8);
+	s1.push(9);
+	s1.push(10);
+	s1.push(11);
+	s1.print();
 	system("pause");
 	return 0;
 }
